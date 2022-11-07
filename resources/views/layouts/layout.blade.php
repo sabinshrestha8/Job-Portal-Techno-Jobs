37345566de1466ae89c7e80ea0608fa7dcf9c311
sabin <sabinshrestha8888@gmail.com>
2022-11-07 15:01:21 +0545

    modified with bootstrap 4.0 cdn link

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,8 +15,7 @@
     inside it is made available to the browser at root level --}}
     <link href="/css/main.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body class="antialiased">
 
@@ -29,5 +28,7 @@ by passing the name of the section we want to yield i.e. content --}}
     Copyright 2022 Blog
 </footer> --}}
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
