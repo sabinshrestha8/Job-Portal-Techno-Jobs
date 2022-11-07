02c0b75c149c982cb043db48ac4bf363f97367a0
sabin <sabinshrestha8888@gmail.com>
2022-11-07 15:21:35 +0545

    created views for jobs i.e. create, index & edit

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container">
+        <div class="text-center mt-3 ">
+            <h1>Create a Job</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-5 mx-auto">
+                <div class="card mt-1 mx-auto p-1 bg-light">
+                    <div class="card-body bg-light">
+                        <div class="container">
+                            <form id="jobs-create-form" method="post" action="{{ route('jobs.store') }}">
+                                @csrf
+                                <div class="controls">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="form_title">Title *</label>
+                                                <input id="form_title" type="text" name="title" class="form-control"
+                                                       placeholder="Java Developer"
+                                                       value="{{ old('title') }}">
+                                                @if($errors->any())
+                                                    <div class="text-danger">
+                                                        {{$errors->first('title')}}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="form_company">Company *</label>
+                                                <input id="form_company" type="text" name="company"
+                                                       class="form-control" placeholder="Leapfrog"
+                                                       value="{{ old('company') }}">
+                                                @if($errors->any())
+                                                    <div class="text-danger">
+                                                        {{$errors->first('company')}}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="form_description">Description *</label>
+                                                <textarea id="form_description" name="description" class="form-control"
+                                                          placeholder="Write a job description here." rows="4">
+                                                          {{old('description') }}</textarea>
+                                                @if($errors->any())
+                                                    <div class="text-danger">
+                                                        {{$errors->first('description')}}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="form_expires_at">Expires at *</label>
+                                                <input id="form_expires_at" type="date" name="expires_at"
+                                                       class="form-control" value="{{ old('expires_at') }}">
+                                                @if($errors->any())
+                                                    <div class="text-danger">
+                                                        {{$errors->first('expires_at')}}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <input type="submit" class="btn btn-success btn-send  pt-2 btn-block
+                                            "value="Save">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.8 -->
+            </div>
+            <!-- /.row-->
+        </div>
+    </div>
+@endsection
---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container">
+        <div class="text-center mt-3">
+            <h1>view All Jobs List</h1>
+        </div>
+        @if(session()->has('msg'))
+            <div class="col-md-4 my-0">
+                <div class="alert alert-success alert-dismissible text-center">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    {{ session()->get('msg') }}
+                </div>
+            </div>
+        @endif
+
+        @if(session()->has('errorMsg'))
+            <div class="col-md-4 my-0">
+                <div class="alert alert-danger alert-dismissible text-center">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    {{ session()->get('errorMsg') }}
+                </div>
+            </div>
+        @endif
+        <div class="container text-center mt-5">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Company</th>
+                    <th>Expires At</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if($jobs)
+                    @foreach($jobs as $job)
+                        <tr>
+                            <td>{{ ++$i }}</td>
+                            <td>{{ $job->title }}</td>
+                            <td>{{ $job->company }}</td>
+                            <td>{{ $job->expires_at }}</td>
+                            <td width="20">
+                                <a href="{{ route('jobs.edit', $job->id) }}" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td width="20">
+                                <form action="{{ route('jobs.destroy', $job->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="6">Jobs not added yet.</td>
+                    </tr>
+                @endif
+                </tbody>
+            </table>
+            {{ $jobs->links() }}
+        </div>
+    </div>
+@endsection
