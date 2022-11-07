39a3b85c5fd76465337447cf4f9bc1bf42c778df
sabin <sabinshrestha8888@gmail.com>
2022-11-07 15:05:27 +0545

    added fillable property on Job Model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'company',
+        'description',
+        'expires_at'
+    ];
 }
