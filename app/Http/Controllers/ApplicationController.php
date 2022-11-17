a9cca7437e2292479d1dc476c80f149eeed3a5b0
sabin <sabinshrestha8888@gmail.com>
2022-11-17 13:09:30 +0545

    modified Login & HomeController & created ApplicationController

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class ApplicationController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $experiences = [
+            'Less than a year',
+            '1-2 years',
+            '2-4 years',
+            '4-7 years',
+            '7-10 years',
+            '10+ years'
+        ];
+
+        return view('apply-form')->with('experiences', $experiences);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $validatedStoreApplication = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'experience' => 'required|string',
+            'message' => 'required',
+            'cv' => 'required|mimes:pdf|max:2048',
+            'remote' => 'required|string',
+            'job_id' => 'required|string'
+        ]);
+
+        if (!$validatedStoreApplication) {
+            return redirect(route('jobs.apply'))
+                    ->withErrors($validatedStoreApplication)->withInput();
+        }
+
+        $applicationArray = [
+            'user_id' => auth()->user()->id,
+            'name' => $validatedStoreApplication['name'],
+            'email' => $validatedStoreApplication['email'],
+            'experience' => $validatedStoreApplication['experience'],
+            'message' => $validatedStoreApplication['message'],
+            'cv' => $validatedStoreApplication['cv'],
+            'remote' => $validatedStoreApplication['remote']
+        ];
+
+        $job = Job::where('id', (int) $validatedStoreApplication['job_id'])->first();
+
+        if(!$job->applications) {
+            $job->applications = [];
+        }
+
+        $filteredApplications = array_filter($job->applications, function ($application) {
+
+            return $application['user_id'] !== auth()->user()->id;
+        });
+
+        $job->applications = [...$filteredApplications, $applicationArray];
+
+        $job->save();
+
+        session()->flash('msg', 'Application submitted successfully');
+
+        return redirect(url('/'));
+    }
+}
