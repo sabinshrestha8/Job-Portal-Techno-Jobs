d2bcb5ac51ec67266e25267a9623aad19c3d82dd
sabin <sabinshrestha8888@gmail.com>
2022-11-28 19:56:09 +0545

    Added search template & show job expired logic

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,11 +6,21 @@
 		<h2 class="text-white fw-bold display-5 mt-2 user-select-none">It's time to start living the life we've imagined.</h2>
 	</div>
 </div>
-<div class="container">
-    <div class="text-center my-5">
+<div class="container my-5">
+    <div class="text-center">
       <h3>Latest Jobs Openings</h3>
       <p class="lead">Create change with your technical expertise</p>
     </div>
+
+    <div class="my-4">
+      <form action="{{ route('jobs.search') }}" class="d-flex justify-content-center">
+        <div class="d-flex form-outline search-container w-50">
+          <input type="text" class="form-control search text-muted fw-bold" name="name" value="{{ request()->name }}" placeholder="Search">
+          <button type="submit" class="btn btn-primary btn-search"><i class="fas fa-search"></i></button>
+        </div>
+      </form>
+    </div>
+
     @foreach ($jobs as $job)
       <div class="card mb-3">
         <div class="card-body">
@@ -18,12 +28,12 @@
             <span class="avatar avatar-text rounded-3 me-4 mb-2" style="background:{{ get_bg_color() }}">{{ get_avatar_text($job->title) }}</span>
             <div class="row flex-fill">
               <div class="col-sm-5">
-                <h4 class="h5"><a href="{{ url('/jobs') . '/' . $job->id}}" class="text-decoration-none link-secondary fw-bold">{{ $job->title }}</a></h4>
-                <span class="badge bg-secondary">{{ $job->location }}</span> <span class="badge bg-success">Rs {{ $job->salary_range }}</span>
+                <h4 class="h5 text-muted"><i class="fas fa-briefcase"></i> <a href="{{ url('/jobs') . '/' . $job->id}}" class="text-decoration-none link-secondary fw-bold">{{ $job->title }}</a></h4>
+                <span class="badge bg-secondary"><i class="fas fa-location"></i> {{ $job->location }}</span> <span class="badge bg-success"><i class="fas fa-rupee-sign"></i> Rs {{ $job->salary_range }}</span>
               </div>
               <div class="col-sm-4 py-2">
                   @foreach ($job->tags as $tag)
-                    <span class="badge bg-secondary">{{ strtoupper($tag) }}</span>
+                    <span class="badge bg-secondary"><i class="fas fa-tag"></i> {{ strtoupper($tag) }}</span>
                   @endforeach
               </div>
               <div class="col-sm-3 text-lg-end">
@@ -50,7 +60,14 @@
                 @else
                   <a href="{{ route('login') }}" class="btn btn-primary" style="width:50%">Apply</a>
                 @endif
-                <h5 class="text-danger mt-3 mb-0 small"><span class="text-muted">Expires at: </span>{{ date('d M, Y', strtotime($job->expires_at)) }}</h5>
+                <h5 class="text-danger mt-3 mb-0 small">
+                    @if(strtotime($job->expires_at) >= time())
+                        <span class="text-muted">Expires at: </span>
+                        {{ date('d M, Y', strtotime($job->expires_at)) }}
+                    @else
+                       <p class="ml-3">{{ "Expired" }}</p>
+                    @endif
+                </h5>
               </div>
             </div>
           </div>
